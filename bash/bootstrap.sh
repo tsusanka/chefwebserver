19a3709b5b5dac971de575c6c338fab266446d63
Tomas Susanka <tsusanka@gmail.com>
2013-12-27 14:19:44 +0100

    Bash: silent curl

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -2,5 +2,5 @@
 
 # check if chef is installed, if not install it
 if ! type "chef-solo" &> /dev/null; then
-	curl -L https://www.opscode.com/chef/install.sh | sudo bash
+	curl -L --silent https://www.opscode.com/chef/install.sh | sudo bash
 fi
