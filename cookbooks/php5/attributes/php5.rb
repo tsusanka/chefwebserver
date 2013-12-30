dd2252b0d5020d3f93b6e0e14059c57900045b0e
Tomas Susanka <tsusanka@gmail.com>
2013-12-30 17:03:15 +0100

    Chef: php.ini file

---
+++ cookbooks/php5/attributes/php5.rb
@@ -0,0 +1,20 @@
+# Attributes for php5's php.ini file. Sets some directives to suit development enviroment.
+# All values taken from "Development values" in php.ini file
+
+default['php']['display_errors'] = 'On'
+default['php']['display_startup_errors'] = 'On'
+default['php']['error_reporting'] = 'E_ALL'
+default['php']['html_errors'] = 'On'
+default['php']['log_errors'] = 'On'
+default['php']['max_input_time'] = '60'
+default['php']['output_buffering'] = '4096'
+default['php']['register_argc_argv'] = 'Off'
+default['php']['request_order'] = '"GP"'
+default['php']['session']['bug_compat_42'] = 'On'
+default['php']['session']['bug_compat_warn'] = 'On'
+default['php']['session']['gc_divisor'] = '1000'
+default['php']['session']['hash_bits_per_character'] = '5'
+default['php']['short_open_tag'] = 'Off'
+default['php']['track_errors'] = 'On'
+default['php']['url_rewriter']['tags'] = '"a=href,area=href,frame=src,input=src,form=fakeentry"'
+default['php']['variables_order'] = '"GPCS"'
--- cookbooks/php5/recipes/default.rb
+++ cookbooks/php5/recipes/default.rb
@@ -8,3 +8,8 @@ package "php5" do
 	action :install
 	notifies :restart, "service[apache2]"
 end
+
+# edit default php.ini config file with ours found in templates/default/php.ini.erb
+template "/etc/php5/apache2/php.ini" do
+	notifies :reload, "service[apache2]"
+end
