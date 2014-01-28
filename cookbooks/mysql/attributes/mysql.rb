9ac1a2cd47ea33414e897fc80800e82b9d67798a
Tomas Susanka <tsusanka@gmail.com>
2014-01-28 10:53:07 +0100

    Chef/MySQL: init connect needs quotes

--- cookbooks/mysql/attributes/mysql.rb
+++ cookbooks/mysql/attributes/mysql.rb
@@ -2,10 +2,10 @@
 
 # For example those four will be populated into my.cnf.erb template.
 # Bind address
-default['mysql']['bind-address'] = '127.0.0.1'
+default['mysql']['bind-address'] = "127.0.0.1"
 
 # Default mysql's character set
-default['mysql']['collation-server'] = 'utf8_unicode_ci'
-default['mysql']['init-connect'] = 'SET NAMES utf8'
-default['mysql']['character-set-server'] = 'utf8'
+default['mysql']['collation-server'] = "utf8_unicode_ci"
+default['mysql']['init-connect'] = "'SET NAMES utf8'"
+default['mysql']['character-set-server'] = "utf8"
 
